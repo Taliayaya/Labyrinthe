6774cfd88f6d680c7392c8a05551b765db0f9fe8
Taliayaya <ilanvinord@gmail.com>
2022-04-12 14:59:28 +0200

    Ajout du package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Labyrinthe',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[])
