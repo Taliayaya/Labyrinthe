33a211d1ead56fc4648132069f7b2214b631f7cf
LaDamedeFer <irene.fioc1@gmail.com>
2022-04-17 18:24:56 +0200

    Ajout de la documentation de la classe graphe

--- labyrinthe/Graph.py
+++ labyrinthe/Graph.py
@@ -1,16 +1,50 @@
-class Graph:
-    def __init__(self):
+class Graph:           # Classe permettant d'implémenter un graphe avec un dictionnaire de listes
+    def __init__(self)->None:
+        u"""
+        Fonction permettant de créer un graphe
+
+        Préconditions:
+            Nom du graphe (self)
+        
+        Postconditions:
+            Aucune
+        """
         self.dico = {}
 
-    def __str__(self):
+    def __str__(self)->str:
+        u"""
+        Fonction permettant l'affichage sous forme de dictionnaire du graphe
+
+        Préconditions:
+            Nom du graphe (self)
+        
+        Postconditions:
+            Contenu du gtraphe (self.graphe)
+        """
         return str(self.dico)
 
-    def addNode(self, node: 'tuple[int]'):
+    def addNode(self, node: 'tuple[int]')->None:
+        u"""
+        Fonction qui permet d'ajouter un noeud dans le graphe
+        
+        Préconditions:
+            Nom du graphe (self) et nom du noeud à ajouter
+        
+        Postconditions:
+            Aucune
+        """
         if node not in self.dico:
             self.dico[node] = []
 
-    def addEdge(self, nodeA: 'tuple[int]', nodeB: 'tuple[int]'):
+    def addEdge(self, nodeA: 'tuple[int]', nodeB: 'tuple[int]')->None:
+        u"""
+        Fonction qui permet d'ajouter une arête dans le graphe entre deux sommets
+        
+        Préconditions:
+            Nom du graphe (self) et noms des noeuds à relier
+        
+        Postconditions:
+            Aucune
+        """
         self.dico[nodeA].append(nodeB)
         self.dico[nodeB].append(nodeA)
-
-# TODO Écrire la documentation
