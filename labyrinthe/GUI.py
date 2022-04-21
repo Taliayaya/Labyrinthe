fcebcb98cebc0bcfba94793a65f27146abc42954
LaDamedeFer <irene.fioc1@gmail.com>
2022-04-21 19:03:50 +0200

    ajout de la documentation concernant les attributs et les méthodes de la classe gui

--- labyrinthe/GUI.py
+++ labyrinthe/GUI.py
@@ -9,20 +9,45 @@ COLOR = 'white'
 
 
 class GUI:
+    u"""
+    Classe permettant de représenter graphiquement un labyrinthe modélisé par un graphe et les différents sommets de ce graphe.
+
+    Attributs:
+    ----------
+        self.board : turtle.Turtle
+            Une fenêtre qui a comme dimensions WIDTH et HEIGHT, la largeur et la hauteur.
+
+    Méthodes:
+    ---------
+        showLabyrinthe(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float = 45)
+            Affiche le labyrinthe rentré en paramètre
+            Attention, le nombre de lignes et de colonnes de ce labyrinthe doit également être précisé en paramètre.
+            La largeur par défaut des cases est de 45 pixels
+
+        drawGraph()
+            méthode à implémenter
+    """
     def __init__(self) -> None:
         self.board = turtle.Turtle()
         turtle.screensize(canvwidth=WIDTH, canvheight=HEIGHT, bg=COLOR)
         turtle.up()
         # turtle.hideturtle()
-        self.setOriginPosition()
+#        self.setOriginPosition()
 
     def showLabyrinthe(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float = 45):
-        self.drawEdge(labyrinthe, nbLine, nbColumn, dist)
-        self.drawLine(labyrinthe, nbLine, nbColumn, dist)
-        self.drawColumn(labyrinthe, nbLine, nbColumn, dist)
+        u"""
+        Fonction permettant d'afficher le labyrinthe rentré en paramètre dans la fenêtre qui apparaît lors de l'appel de la classe
+        Préconditions:
+            labyrinthe : G
+                un objet de la classe graphe ayant des tuples en tant que sommets
+
+        """
+        self.__drawEdge(labyrinthe, nbLine, nbColumn, dist)
+        self.__drawLine(labyrinthe, nbLine, nbColumn, dist)
+        self.__drawColumn(labyrinthe, nbLine, nbColumn, dist)
         turtle.done()
 
-    def drawLine(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float):
+    def __drawLine(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float):
         for l in range(1, nbLine):
             turtle.up()
             turtle.setposition(-nbColumn*dist/2, (nbLine*dist/2) - l*dist)
@@ -33,7 +58,7 @@ class GUI:
                 turtle.forward(dist)
         turtle.up()
 
-    def drawColumn(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float):
+    def __drawColumn(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float):
         turtle.right(90)
         for c in range(1, nbColumn):
             turtle.up()
@@ -45,7 +70,7 @@ class GUI:
                 turtle.forward(dist)
         turtle.up()
 
-    def drawEdge(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float):
+    def __drawEdge(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float):
         turtle.setposition(-nbColumn*dist/2, -nbLine*dist/2)
         turtle.down()
         turtle.setposition(-nbColumn*dist/2, nbLine*dist/2)
@@ -58,7 +83,6 @@ class GUI:
     def setOriginPosition(self):
         turtle.home()
 
-    # TODO écris moi la fonction dessin de la partie 2 :p
 
 
 if __name__ == "__main__":
