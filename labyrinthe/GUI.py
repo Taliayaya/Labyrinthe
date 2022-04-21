eae95645424d03efd1172b439bcd9ecbe907c69f
LaDamedeFer <irene.fioc1@gmail.com>
2022-04-21 18:42:21 +0200

    ajout des fonctions pour dessiner le labyrinthe

--- labyrinthe/GUI.py
+++ labyrinthe/GUI.py
@@ -1,4 +1,6 @@
 import turtle
+import Graph as G
+import Labyrinthe
 
 WIDTH = 800
 HEIGHT = 800
@@ -8,31 +10,57 @@ COLOR = 'white'
 
 class GUI:
     def __init__(self) -> None:
+        turtle.speed(1)
         self.board = turtle.Turtle()
         turtle.screensize(canvwidth=WIDTH, canvheight=HEIGHT, bg=COLOR)
         turtle.up()
         # turtle.hideturtle()
         self.setOriginPosition()
 
-    def showLabyrinthe(self, nbLine: int, nbColumn: int, dist: float):
-        self.drawLine(nbLine, nbColumn, dist)
-        self.drawColumn(nbLine, nbColumn, dist)
+    def showLabyrinthe(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float = 45):
+        self.drawEdge(labyrinthe, nbLine, nbColumn, dist)
+        self.drawLine(labyrinthe, nbLine, nbColumn, dist)
+        self.drawColumn(labyrinthe, nbLine, nbColumn, dist)
         turtle.done()
 
-    def drawLine(self, nbLine: int, nbColumn: int, dist: float):
-        pass
+    def drawLine(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float):
+        for l in range(1, nbLine):
+            turtle.up()
+            turtle.setposition(-nbColumn*dist/2, (nbLine*dist/2) - l*dist)
+            for c in range(1, nbColumn+1):
+                turtle.up()
+                if (l+1, c) not in labyrinthe.dico[(l, c)]:
+                    turtle.down()
+                turtle.forward(dist)
+        turtle.up()
 
-    def drawColumn(self, nbLine: int, nbColumn: int, dist: float):
-        pass
+    def drawColumn(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float):
+        turtle.right(90)
+        for c in range(1, nbColumn):
+            turtle.up()
+            turtle.setposition(-nbColumn*dist/2 + c*dist, (nbLine*dist/2))
+            for l in range(1, nbLine+1):
+                turtle.up()
+                if (l, c+1) not in labyrinthe.dico[(l, c)]:
+                    turtle.down()
+                turtle.forward(dist)
+        turtle.up()
 
-    def drawEdge(self, nbLine: int, nbColumn: int, dist: float):
-        pass
+    def drawEdge(self, labyrinthe: G, nbLine: int, nbColumn: int, dist: float):
+        turtle.setposition(-nbColumn*dist/2, -nbLine*dist/2)
+        turtle.down()
+        turtle.setposition(-nbColumn*dist/2, nbLine*dist/2)
+        turtle.setposition(nbColumn*dist/2, nbLine*dist/2)
+        turtle.setposition(nbColumn*dist/2, -nbLine*dist/2)
+        turtle.setposition(-nbColumn*dist/2, -nbLine*dist/2)
+        turtle.up()
+
 
     def setOriginPosition(self):
-        turtle.setposition(-WIDTH//2, HEIGHT//2)
+        turtle.home()
 
     # TODO écris moi la fonction dessin de la partie 2 :p
 
 
 if __name__ == "__main__":
-    GUI().drawGraph()
+    GUI().showLabyrinthe(Labyrinthe.Labyrinthe, 4, 8)
