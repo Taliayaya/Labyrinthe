da3849cc68429cf25b2af6f5bfa8e5fff0baa4ac
Taliayaya <ilanvinord@gmail.com>
2022-05-16 23:16:53 +0200

    removed useless sleep

--- labyrinthe/__main__.py
+++ labyrinthe/__main__.py
@@ -4,10 +4,8 @@
 import labyrinthe.GUI as G
 import labyrinthe.Labyrinthe as L
 import turtle
-import time
 turtle.hideturtle()
 main = G.GUI()
-time.sleep(5)
 labyrinthe = L.Labyrinthe
 main.showLabyrinthe(labyrinthe, 4, 8)
 main.showGraph(labyrinthe, 4, 8)
