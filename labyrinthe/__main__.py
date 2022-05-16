031b2ce7a4e92aeb9f60e8e8db7a8d7612c50578
Taliayaya <ilanvinord@gmail.com>
2022-05-16 23:10:00 +0200

    Fichier main

--- labyrinthe/__main__.py
+++ labyrinthe/__main__.py
@@ -3,4 +3,14 @@
 
 import labyrinthe.GUI as G
 import labyrinthe.Labyrinthe as L
-G.GUI().showLabyrinthe(L.Labyrinthe, 4, 8)
+import turtle
+import time
+turtle.hideturtle()
+main = G.GUI()
+time.sleep(5)
+labyrinthe = L.Labyrinthe
+main.showLabyrinthe(labyrinthe, 4, 8)
+main.showGraph(labyrinthe, 4, 8)
+main.drawParcours(labyrinthe)
+main.showChemin(labyrinthe)
+turtle.done()
